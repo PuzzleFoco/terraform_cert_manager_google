a196597a975bc5fe7ad7040a9fdd51f9b4d498a8
FabiusE <fabius.engel@msg.group>
2020-06-10 21:55:28 +0200

    feat: cert-secret with service account

--- main.tf
+++ main.tf
@@ -43,16 +43,34 @@ resource "null_resource" "install_crds" {
 }
 
 // Creates a JSON File with the credentials of the Google IAM-Account
-resource "null_resource" "create_key_json" {
-  provisioner "local-exec" {
-    when    = create
-    command = "gcloud iam service-accounts keys create ${path.module}/key.json --iam-account ${var.iam_account}"
+resource "google_service_account" "certaccount" {
+  account_id   = "certaccount"
+  display_name = "Cert Service Account"
+}
+
+resource "google_service_account_key" "certkey" {
+  service_account_id = google_service_account.certaccount.name
+}
+
+resource "kubernetes_secret" "cert-manager-secret" {
+  metadata {
+    name = "cert-credentials"
+    namespace = kubernetes_namespace.cert_manager.metadata.0.name
   }
-  provisioner "local-exec" {
-    when    = destroy
-    command = "private_key_id=$(jq -r .private_key_id ${path.module}/key.json) && client_email=$(jq -r .client_email ${path.module}/key.json) && gcloud iam service-accounts keys delete $private_key_id --iam-account $client_email --quiet && truncate -s 0 ${path.module}/key.json"
+  data = {
+    "key.json" = base64decode(google_service_account_key.mykey.private_key)
   }
 }
+# resource "null_resource" "create_key_json" {
+#   provisioner "local-exec" {
+#     when    = create
+#     command = "gcloud iam service-accounts keys create ${path.module}/key.json --iam-account ${var.iam_account}"
+#   }
+#   provisioner "local-exec" {
+#     when    = destroy
+#     command = "private_key_id=$(jq -r .private_key_id ${path.module}/key.json) && client_email=$(jq -r .client_email ${path.module}/key.json) && gcloud iam service-accounts keys delete $private_key_id --iam-account $client_email --quiet && truncate -s 0 ${path.module}/key.json"
+#   }
+# }
 
 // Adds jetsteck to helm repo
 data "helm_repository" "jetstack" {
@@ -70,24 +88,24 @@ resource "helm_release" "cert-manager" {
   version    = local.certManagerHelmVersion
 }
 
-data "template_file" "cert_secret" {
-  template  = file("${path.module}/key.json")
+# data "template_file" "cert_secret" {
+#   template  = file("${path.module}/key.json")
 
-  depends_on = [null_resource.create_key_json]
-}
+#   depends_on = [null_resource.create_key_json]
+# }
 
 // Creates secret with our client_secret inside. Is used to give cert-manager the permission to make an  acme-challenge to prove let's encrypt
 // that we are the owner of our domain
-resource "kubernetes_secret" "cert-manager-secret" {
-  metadata {
-    name      = "secret-google-config"
-    namespace = kubernetes_namespace.cert_manager.metadata.0.name
-  }
-  type = "Opaque"
-  data = {
-    "key.json" = data.template_file.cert_secret.template
-  }
-}
+# resource "kubernetes_secret" "cert-manager-secret" {
+#   metadata {
+#     name      = "secret-google-config"
+#     namespace = kubernetes_namespace.cert_manager.metadata.0.name
+#   }
+#   type = "Opaque"
+#   data = {
+#     "key.json" = data.template_file.cert_secret.template
+#   }
+# }
 
 // Creates a template file with all necessary variables for permission. This template contains a clusterissuer and a certificate
 data "template_file" "cert_manager_manifest" {
