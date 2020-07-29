0fa4442ae1542f041c9de756bf947ed9af46adaf
Fabius Engel <Fabius.Engel@msg.group>
2020-07-29 21:47:59 +0200

    remove comment

--- output.tf
+++ output.tf
@@ -1,13 +1,3 @@
-############################################################################
-# Created Date: 29.10.2019                                                 #
-# Author: Michael Süssemilch (michael.suessemilch@msg.group)               #
-# -----                                                                    #
-# Last Modified: 30.10.2019 10:38:45                                       #
-# Modified By: Michael Süssemilch (michael.suessemilch@msg.group)          #
-# -----                                                                    #
-# Copyright (c) 2019 msg nexinsure ag                                      #
-############################################################################
-
 output "cert_domain" {
   value = "${var.root_domain}"
 }
